b44289336db3474323e2b00a44622ec7b630d240
Brayan <brayan.057@live.cl>
2020-10-27 04:12:38 -0300

    cargando en git

--- manage.py
+++ manage.py
@@ -4,8 +4,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_lider.settings.local')
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_lider.settings.production')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_lider.settings.local')
+    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyecto_lider.settings.production')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
